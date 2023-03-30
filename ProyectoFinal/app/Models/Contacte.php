9acff01afe8e728a20ed943b33d3d04f4637bebf
jmiguelcn <113700439+jmiguelcn@users.noreply.github.com>
2023-03-30 16:53:59 +0200

    Llista i formularis de contactes
    - list.blade.php agregat per Contacte
    - new.blade.php agregat per Contacte
    - edit.blade.php agregat per Contacte

--- ProyectoFinal/app/Models/Contacte.php
+++ ProyectoFinal/app/Models/Contacte.php
@@ -4,8 +4,14 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\DB;
 
 class Contacte extends Model
 {
     use HasFactory;
+
+    public function empresa()
+    {
+        return $this->belongsTo(Empresa::class);
+    }
 }
