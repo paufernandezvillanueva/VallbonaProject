afe0e0d2215c681138154cbc7c48243b9a8f00fd
fernandezpau <pau_fernandezvillanueva@iescarlesvallbona.cat>
2023-04-11 17:39:41 +0200

    Relació Empresa-Contacte

--- ProyectoFinal/app/Models/Empresa.php
+++ ProyectoFinal/app/Models/Empresa.php
@@ -4,8 +4,14 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Empresa extends Model
 {
     use HasFactory;
+
+    public function contacte()
+    {
+        return $this->hasMany(Contacte::class);
+    }
 }
