9cee989bc8226695b1ea351f22aeaf8ea8dc3a64
fernandezpau <pau_fernandezvillanueva@iescarlesvallbona.cat>
2023-04-11 17:44:09 +0200

    Relació Usuari-Estada

--- ProyectoFinal/app/Models/User.php
+++ ProyectoFinal/app/Models/User.php
@@ -4,7 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class User extends Model
 {
@@ -12,6 +12,6 @@ class User extends Model
     
     public function estada()
     {
-        return $this->belongsTo(Estada::class);
+        return $this->hasMany(Estada::class);
     }
 }
