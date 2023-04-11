4b32c75e97f5a0c92c218d67acf0081240bef709
fernandezpau <pau_fernandezvillanueva@iescarlesvallbona.cat>
2023-04-11 17:22:05 +0200

    Update User.php

--- ProyectoFinal/app/Models/User.php
+++ ProyectoFinal/app/Models/User.php
@@ -2,11 +2,14 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class User extends Model
 {
+    use HasFactory;
+    
     public function estada()
     {
         return $this->belongsTo(Estada::class);
