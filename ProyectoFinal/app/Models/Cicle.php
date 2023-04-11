32fa043400eaeaf53d825c8f765087d459cb509e
fernandezpau <pau_fernandezvillanueva@iescarlesvallbona.cat>
2023-04-11 17:35:36 +0200

    Relacions Cicle-Estada

--- ProyectoFinal/app/Models/Cicle.php
+++ ProyectoFinal/app/Models/Cicle.php
@@ -4,8 +4,14 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Cicle extends Model
 {
     use HasFactory;
+
+    public function estada()
+    {
+        return $this->belongsTo(Estada::class);
+    }
 }
