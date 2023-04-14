010bb7f6fe18d45fbb7db3a1718dc33ad24fb3c5
fernandezpau <pau_fernandezvillanueva@iescarlesvallbona.cat>
2023-04-14 19:07:41 +0200

    css puesto en archivo aparte y

--- ProyectoFinal/resources/views/navbar.blade.php
+++ ProyectoFinal/resources/views/navbar.blade.php
@@ -1,39 +1,51 @@
 <div class="d-flex flex-column flex-shrink-0 p-3 text-white bg-dark" style="width: 200px; height: 100vh;">
     <a href="{{ asset('/') }}" class="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-white text-decoration-none">
-        <svg class="bi me-2" width="40" height="32"><use xlink:href="#bootstrap"/></svg>
+        <svg class="bi me-2" width="40" height="32">
+            <use xlink:href="#bootstrap" />
+        </svg>
         <span class="fs-4">
-                    <img src=" {{ asset ('/img/logo-vallbona.png' )}}" alt="Vallbona" height="30px" >
-                    CARLES VALLBONA</span>
+            <img src=" {{ asset ('/img/logo-vallbona.png' )}}" alt="Vallbona" height="30px">
+            CARLES VALLBONA</span>
     </a>
     <hr>
     <ul class="nav nav-pills flex-column mb-auto">
         <li class="nav-item">
             <a href="{{ asset('/') }}" class="nav-link text-white" aria-current="page">
-                <svg class="bi me-2" width="16" height="16"><use xlink:href="#home"/></svg>
+                <svg class="bi me-2" width="16" height="16">
+                    <use xlink:href="#home" />
+                </svg>
                 Empreses
             </a>
         </li>
         <li>
             <a href="#" class="nav-link text-white">
-                <svg class="bi me-2" width="16" height="16"><use xlink:href="#table"/></svg>
+                <svg class="bi me-2" width="16" height="16">
+                    <use xlink:href="#table" />
+                </svg>
                 Cicles
             </a>
         </li>
         <li>
             <a href="#" class="nav-link text-white">
-                <svg class="bi me-2" width="16" height="16"><use xlink:href="#grid"/></svg>
+                <svg class="bi me-2" width="16" height="16">
+                    <use xlink:href="#grid" />
+                </svg>
                 Cursos
             </a>
         </li>
         <li>
             <a href="{{ asset('/contacte/list') }}" class="nav-link text-white">
-                <svg class="bi me-2" width="16" height="16"><use xlink:href="#people-circle"/></svg>
+                <svg class="bi me-2" width="16" height="16">
+                    <use xlink:href="#people-circle" />
+                </svg>
                 Contactes
             </a>
         </li>
         <li>
             <a href="{{ asset('/user/list') }}" class="nav-link text-white">
-                <svg class="bi me-2" width="16" height="16"><use xlink:href="#people-circle"/></svg>
+                <svg class="bi me-2" width="16" height="16">
+                    <use xlink:href="#people-circle" />
+                </svg>
                 Usuaris
             </a>
         </li>
@@ -47,7 +59,9 @@
         <ul class="dropdown-menu dropdown-menu-dark text-small shadow" aria-labelledby="dropdownUser1">
             <li><a class="dropdown-item" href="#">Configuracio</a></li>
             <li><a class="dropdown-item" href="#">Perfil</a></li>
-            <li><hr class="dropdown-divider"></li>
+            <li>
+                <hr class="dropdown-divider">
+            </li>
             <li><a class="dropdown-item" href="#">Logout</a></li>
         </ul>
     </div>
