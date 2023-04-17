7ec01d48a22a87dcca8dc6a8aa2a3134fce2a331
fernandezpau <pau_fernandezvillanueva@iescarlesvallbona.cat>
2023-04-17 19:06:56 +0200

    Cambiado el boton de añadir y añadido modal eliminar

--- ProyectoFinal/resources/views/contacte/list.blade.php
+++ ProyectoFinal/resources/views/contacte/list.blade.php
@@ -1,41 +1,17 @@
-<style>
-    .modal-body>.row {
-        margin-top: 5px;
-        margin-left: 10px;
-    }
-
-    @media screen and (max-width: 575px) {
-        label {
-            float: left;
-        }
-    }
-
-    @media screen and (min-width: 576px) {
-        label {
-            float: right;
-        }
-    }
-
-    #icon-basura {
-        font-size: larger;
-    }
-
-    #icon-basura:hover {
-        color: red;
-    }
-</style>
 @extends('layout')
 
 @section('title', 'Llistat de contactes')
 
 @section('stylesheets')
 @parent
+<link rel="stylesheet" href="{{ asset('css/userList.css') }}" />
 @endsection
 
 @section('content')
 <div class="titulo">
     <h1>Llista de contactes</h1>
 </div>
+
 <div class="modal fade" id="nouContacte" tabindex="-1" aria-labelledby="nouContacteLabel" aria-hidden="true">
     <div class="modal-dialog modal-dialog-centered modal-lg">
         <div class="modal-content">
@@ -54,17 +30,17 @@
                             <input class="form-control" type="text" name="name" />
                         </div>
                     </div>
-
-                <div class="row">
-                    <div class="col-md-2 col-sm-2">
-                        <label class="col-form-label text-dark" for="empresa_id">Empresa</label>
-                    </div>
-                    <div class="col-md-10 col-sm-10">
-                        <select class="form-select" name="empresa_id">
-                            @foreach ($empresas as $empresa)
+                    <div class="row">
+                        <div class="col-md-2 col-sm-2">
+                            <label class="col-form-label text-dark" for="empresa_id">Empresa</label>
+                        </div>
+                        <div class="col-md-10 col-sm-10">
+                            <select class="form-select" name="empresa_id">
+                                @foreach ($empresas as $empresa)
                                 <option value="{{ $empresa->id }}">{{ $empresa->name }}</option>
-                            @endforeach
-                        </select>
+                                @endforeach
+                            </select>
+                        </div>
                     </div>
                     <div class="row">
                         <div class="col-md-2 col-sm-2">
@@ -83,7 +59,6 @@
                         </div>
                     </div>
                 </div>
-                </div>
                 <div class="modal-footer">
                     <button type="button" class="btn btn-default" data-bs-dismiss="modal">Cancelar</button>
                     <button type="submit" class="btn btn-primary">Confirmar</button>
@@ -99,32 +74,62 @@
     <strong>Success!</strong> {{ session('status') }}
 </div>
 @endif
-<div class="table-responsive" style=" height: 80vh; margin: auto ">
-    <table class="table table-striped table-dark " style="margin-top: 20px;margin-bottom: 10px; -webkit-overflow-scrolling: auto">
+
+<table id="user-table" class="table table-striped table-dark">
     <thead>
         <tr>
             <th scope="col">Nom</th>
             <th scope="col">Empresa</th>
             <th scope="col">Email</th>
             <th scope="col">Telefon</th>
-            <th><a href="#" id="btnAfegirContacte" data-bs-toggle="modal" data-bs-target="#nouContacte">Nou contacte</a></th>
+            <th><a href="#" id="btnAfegirContacte" data-bs-toggle="modal" data-bs-target="#nouContacte"><i class="bi bi-plus-square-fill"></i></a></th>
         </tr>
     </thead>
     <tbody>
         @foreach ($contactes as $contacte)
         <tr>
-            <th scope="row">{{ $contacte->name }}</th>
+            <td>{{ $contacte->name }}</td>
             <td>{{ $contacte->empresa->name }}</td>
             <td>{{ $contacte->email }}</td>
             <td>{{ $contacte->phonenumber }}</td>
             <td>
-                <a href="{{ route('contacte_delete', ['id' => $contacte->id]) }}" id="icon-basura"><i class="bi bi-trash3-fill"></i></a>
-                <a href="{{ route('contacte_edit', ['id' => $contacte->id]) }}">Editar</a>
+                <a data-id="{{ $contacte->id }}" class="iconBasura" data-bs-toggle="modal" data-bs-target="#confirmDelete">
+                    <i class="bi bi-trash3-fill"></i>
+                </a>
+                <!-- <a href="{{ route('contacte_edit', ['id' => $contacte->id]) }}">Editar</a> -->
             </td>
         </tr>
         @endforeach
     </tbody>
 </table>
+
+<div class="modal fade" id="confirmDelete" tabindex="-1" aria-labelledby="confirmDeleteLabel" aria-hidden="true">
+    <div class="modal-dialog modal-dialog-centered">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="confirmDeleteLabel">Eliminar contacte</h5>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <form method="GET">
+                <div class="modal-body">
+                    <script>
+                        document.querySelectorAll('.iconBasura').forEach(elem => {
+                            elem.addEventListener('click', () => {
+                                var dataId = elem.dataset.id;
+                                var form = document.querySelector('#confirmDelete form');
+                                form.action = "delete/" + dataId;
+                            });
+                        });
+                    </script>
+                    @csrf
+                    <p>Estàs segur de voler eliminar aquest contacte?</p>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-danger" data-bs-dismiss="modal">Cancelar</button>
+                    <button type="submit" class="btn btn-success" id="btnConfirmar">Confirmar</button>
+                </div>
+            </form>
+        </div>
+    </div>
 </div>
-<br>
 @endsection
