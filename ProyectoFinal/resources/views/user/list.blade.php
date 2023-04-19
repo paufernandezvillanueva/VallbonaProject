742b22a171b77be9ce1c96a239b83dfc9565123d
fernandezpau <pau_fernandezvillanueva@iescarlesvallbona.cat>
2023-04-19 16:25:14 +0200

    Añadido opcion "selecciona ciclo" y "selecciona rol"

--- ProyectoFinal/resources/views/user/list.blade.php
+++ ProyectoFinal/resources/views/user/list.blade.php
@@ -52,6 +52,7 @@
                         </div>
                         <div class="col-md-10 col-sm-10">
                             <select class="form-select" name="cicle_id">
+                                <option>Selecciona un cicle...</option>
                                 @foreach ($cicles as $cicle)
                                 <option value="{{ $cicle->id }}">{{ $cicle->shortname }} - {{ $cicle->name }}</option>
                                 @endforeach
@@ -64,6 +65,7 @@
                         </div>
                         <div class="col-md-10 col-sm-10">
                             <select class="form-select" name="rol_id">
+                            <option>Selecciona un rol...</option>
                                 @foreach ($rols as $rol)
                                 <option value="{{ $rol->id }}">{{ $rol->name }}</option>
                                 @endforeach
