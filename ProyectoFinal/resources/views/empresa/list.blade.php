6af91974fbe709f86e760fe0154a08ed130138ec
jmiguelcn <113700439+jmiguelcn@users.noreply.github.com>
2023-05-03 19:39:34 +0200

    Cambios de diseño

--- ProyectoFinal/resources/views/empresa/list.blade.php
+++ ProyectoFinal/resources/views/empresa/list.blade.php
@@ -181,7 +181,7 @@
             <th>Nom</th>
             <th>Sector</th>
             <th>Població</th>
-            <th>Nº Estades</th>
+            <th>Estades</th>
             <th>Valoració</th>
             <th>Contactes</th>
             <th>
