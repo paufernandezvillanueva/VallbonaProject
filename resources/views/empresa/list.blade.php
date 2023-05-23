0c077685cb02aba744de2def8408c801ac92e373
fernandezpau <pau_fernandezvillanueva@iescarlesvallbona.cat>
2023-05-23 18:42:31 +0200

    Arreglo filtro de los campos con minimo y maximo (gutter)

--- resources/views/empresa/list.blade.php
+++ resources/views/empresa/list.blade.php
@@ -131,7 +131,7 @@
                 <label for="estadas">{{ trans('translation.estades') }}:</label>
             </div>
             <div class="col-lg-4 col-9">
-                <div class="row">
+                <div class="row g-0">
                     <div class="col-lg-5 col-5">
                         @if (isset($request->minEstadas) && $request->minEstadas != "")
                         <input class="form-control" type="number" id="minEstadas" placeholder="{{ trans('translation.min') }}" name="minEstadas" min="0" value="{{ $request->minEstadas }}" />
@@ -153,7 +153,7 @@
                 <label for="valoracio">{{ trans('translation.valoration') }}:</label>
             </div>
             <div class="col-lg-4 col-9">
-                <div class="row">
+                <div class="row g-0">
                     <div class="col-lg-5 col-5">
                         @if (isset($request->minValoracio) && $request->minValoracio != "")
                         <input class="form-control" type="number" id="minValoracio" placeholder="{{ trans('translation.min') }}" name="minValoracio" min="0" max="10" value="{{ $request->minValoracio }}" />
