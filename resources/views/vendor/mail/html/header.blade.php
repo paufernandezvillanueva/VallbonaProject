f6e9ccfad0954f09a99637c0006a8fedc3703c10
jmiguelcn <113700439+jmiguelcn@users.noreply.github.com>
2023-05-23 15:26:15 +0200

    Update header.blade.php

--- resources/views/vendor/mail/html/header.blade.php
+++ resources/views/vendor/mail/html/header.blade.php
@@ -2,11 +2,7 @@
 <tr>
 <td class="header">
 <a href="{{ $url }}" style="display: inline-block;">
-@if (trim($slot) === 'Laravel')
 <img src="{{ asset('img/logo-vallbona.png') }}" class="logo" alt="Vallbona Logo">
-@else
-{{ $slot }}
-@endif
 </a>
 </td>
 </tr>
